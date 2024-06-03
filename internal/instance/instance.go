7258f252802bb9ee88aea085bcdbcee4161fbd99
starx <i@starx.ink>
2024-06-03 14:49:41 +0800

    fix: api may not return the metric data on first day of month

--- internal/instance/instance.go
+++ internal/instance/instance.go
@@ -16,22 +16,30 @@ type Instance struct {
 }
 
 func (x *Instance) Check() {
-	newAPI := api.NewAPI(x.Name)
-	var total int
 	log.Println("----------")
 	log.Printf("Instance Name: %s", x.Name)
+
+	newAPI := api.NewAPI(x.Name)
+	total := 0
 	for _, name := range MetricNames {
 		md, err := newAPI.MetricDataMonth(name)
 		if err != nil {
 			log.Printf("Get data of metric: %s failed, err=%s", name, err)
 			continue
 		}
-		used := int(x.Limit.Unit.BytesToUnit(md.Data[0].Sum))
+		used := 0
+		if len(md.Data) > 0 {
+			used = int(x.Limit.Unit.BytesToUnit(md.Data[0].Sum))
+		}
 		total += used
 		log.Printf("Metric: %s Used: %d %s\n", name, used, x.Limit.Unit)
 	}
 	log.Printf("Total Used: %d %s", total, x.Limit.Unit)
-	if total >= x.Limit.Value {
+
+	if total < x.Limit.Value {
+		left := x.Limit.Value - total
+		log.Printf("Traffic Left: %d %s (%.2f %%)", left, x.Limit.Unit, float32(left)/float32(x.Limit.Value)*100)
+	} else {
 		overflow := total - x.Limit.Value
 		if overflow > 0 {
 			log.Printf("Overflow: %d %s (%.2f %%)", overflow, x.Limit.Unit, float32(total)/float32(x.Limit.Value)*100)
@@ -40,9 +48,6 @@ func (x *Instance) Check() {
 		x.ExecuteCommand()
 		log.Println("Exiting..")
 		os.Exit(0)
-	} else {
-		left := x.Limit.Value - total
-		log.Printf("Traffic Left: %d %s (%.2f %%)", left, x.Limit.Unit, float32(left)/float32(x.Limit.Value)*100)
 	}
 	log.Println("----------")
 }
